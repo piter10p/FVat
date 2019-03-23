afcdeda78c03e058b1cfc370832a3e512ff0071f
Piotr Paszko <piter9p@interia.pl>
2019-03-23 12:19:56 +0100

    Implemented start window.

--- FVat/FVat/Views/StartWindow.xaml.cs
+++ FVat/FVat/Views/StartWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FVat
+namespace FVat.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for StartWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class StartWindow : Window
     {
-        public MainWindow()
+        public StartWindow()
         {
             InitializeComponent();
         }
